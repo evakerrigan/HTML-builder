638ffd03a79a3f580fab76e830541a5774d7f6f3
Eva Kerrigan <evakerrigan@gmail.com>
2022-05-24 05:05:55 +0300

    feat: 3 task done

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,39 +8,17 @@ fs.readdir(filePath, (err, files) => {
   if (err) {
     throw err;
   }
- 
-
-    files.forEach((file) => {
-
-      const fullPath =  `${filePath}/${file}`
-      console.log(fullPath);
-
-      fs.stat(fullPath, (err, stats) => {
-        if (err) {
-          throw err;
-        }
-
-      if (stats.isFile()) {
-
-        //console.log(stats);
-
-        let name = file.split(".");
-        let size = stats.size;
-
-        //stdout.write(file);
-        stdout.write(`\n`);
-        stdout.write(name[0]);
-        stdout.write(`\n`);
-        stdout.write(name[1]);
-        stdout.write(size);
-
-        //stdout.write('${name} - ${ext} - ${size} 'bite' \n');
+  files.forEach((file) => {
+    const fullPath = `${filePath}/${file}`;
+    fs.stat(fullPath, (err, stats) => {
+      if (err) {
+        throw err;
       }
-
-      //console.log(stats);
-
-
-
+      if (stats.isFile()) {
+        let name = file.split(".");
+        let size = stats.size.toString();
+        stdout.write(`${name[0]} - ${name[1]} - ${size} bite \n`);
+      }
     });
   });
 });
